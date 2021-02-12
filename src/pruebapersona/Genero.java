df5214b583d6de6488f99b340c225621e06e7814
Jozzelu <joseluis.fdez92@gmail.com>
2021-02-12 11:19:34 +0100

    New class Genero was added

---
+++ src/pruebapersona/Genero.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebapersona;
+
+/**
+ * 
+ * @author Jozzelu
+ */
+public class Genero {
+    
+    // Esto es una clase nueva para comprobar que sigo creando nuevos cambios
+
+}
